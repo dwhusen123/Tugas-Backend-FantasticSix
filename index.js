dfc2ffb6d18964272e6f32da53dd84a6cdbb2fd6
husendw <136424221+dwhusen123@users.noreply.github.com>
2024-12-09 23:55:28 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express"
+import dotenv from  "dotenv"
+import { connection } from "./database/db.js";
+import route from "./routes/route.js"
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(route);
+
+app.listen(process.env.PORT, async()=> {
+    await connection();
+    console.log(`https://localhost:${process.env.PORT}`)
+})
